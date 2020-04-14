16563a720a85e2df0723c7a11f3904ffee22eff7
zikwall <andrey.kapitonov.96@gmail.com>
2020-04-14 11:29:51 +0300

    Add CLI App runner with export formats

--- main.go
+++ main.go
@@ -4,31 +4,61 @@ import (
 	"fmt"
 	"github.com/LimeHD/parser/base"
 	"github.com/LimeHD/parser/parsers"
+	"github.com/urfave/cli"
+	"log"
+	"os"
 )
 
 func main() {
-	epg := base.Epg{Days: map[string]*base.Day{}}
-	parser := &base.Parser{}
-
-	// 5 - это для теста, сколько дней вперед парсить, включая текущий день
-	for i := 0; i <= 5; i++ {
-		key := fmt.Sprintf("Day - %d", i)
-
-		if !epg.DayExist(key) {
-			epg.AppendDay(key, &base.Day{
-				Name:   key,
-				Common: &base.Common{},
-			})
-		}
-
-		digea := parsers.Digea{}
-		digea.SetBaseUrl(fmt.Sprintf("https://www.digea.gr/EPG?day=%d", i))
-
-		parser.RunComposeWith(&digea, i)
-		epg.Days[key].Common = &digea.Common
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "format",
+				Value: "csv",
+				Usage: "Export data format",
+			},
+		},
 	}
 
-	for _, v := range epg.Days["Day - 3"].ToCSV() {
-		fmt.Println(v)
+	app.Action = func(c *cli.Context) error {
+		format := c.String("format")
+
+		epg := base.Epg{Days: map[string]*base.Day{}}
+		parser := &base.Parser{}
+
+		// 5 - это для теста, сколько дней вперед парсить, включая текущий день
+		for i := 0; i <= 5; i++ {
+			key := fmt.Sprintf("Day - %d", i)
+
+			if !epg.DayExist(key) {
+				epg.AppendDay(key, &base.Day{
+					Name:   key,
+					Common: &base.Common{},
+				})
+			}
+
+			digea := parsers.Digea{}
+			digea.SetBaseUrl(fmt.Sprintf("https://www.digea.gr/EPG?day=%d", i))
+
+			parser.RunComposeWith(&digea, i)
+			epg.Days[key].Common = &digea.Common
+		}
+
+		// todo пока что временно так, потом заревакторю
+		if format == "csv" {
+			for _, v := range epg.Days["Day - 3"].ToCSV() {
+				fmt.Println(v)
+			}
+		} else {
+			fmt.Println(epg.Days["Day - 3"].Common.Marshal())
+		}
+
+		return nil
+	}
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
 	}
 }
