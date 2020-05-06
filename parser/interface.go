c6a9f4d9d11b7a4b5fd200e44cf8252c39c3c755
zikwall <andrey.kapitonov.96@gmail.com>
2020-05-06 13:23:22 +0300

    Подправил внешний вид для читабельности

--- parser/interface.go
+++ parser/interface.go
@@ -4,9 +4,9 @@ import "github.com/PuerkitoBio/goquery"
 
 // main interface, every parser must implement it
 type IParse interface {
-	BaseUrl() string
 	Parse(doc *goquery.Document, day int)
+	BaseUrl() string
 	Marshal() string
-	BootstrapLocalTime(local string) error
 	GetLocalTime() string
+	BootstrapLocalTime(local string) error
 }
