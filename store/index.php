4f1bef693832df041196a60bc7d40697df6ff1d9
zikwall <andrey.kapitonov.96@gmail.com>
2020-05-19 11:49:26 +0300

    Отрефакторил стуктуру проекта парсера

--- store/index.php
+++ store/index.php
@@ -1,12 +1,12 @@
 <?php ini_set('memory_limit','2048M');
 
 require_once 'vendor/autoload.php';
-require_once 'StorageInterface.php';
-require_once 'Fmt.php';
-require_once 'Datamapper.php';
-require_once 'Query.php';
-require_once 'Storage.php';
-require_once 'FileHelper.php';
+require_once 'src/StorageInterface.php';
+require_once 'src/Fmt.php';
+require_once 'src/Datamapper.php';
+require_once 'src/Query.php';
+require_once 'src/Storage.php';
+require_once 'src/FileHelper.php';
 
 $options    = getopt("h:l:d:p:i:g");
 $connection = new Query($options);
@@ -17,7 +17,7 @@ $groupedDb  = $storage->getEqualItemsFor($days['items']);
 
 foreach ($days['items'] as $key => $day) {
     foreach ($day as $channel => $programs) {
-        
+
         if (!Datamapper::isEqualMaps($programs, $groupedDb[$key][$channel])) {
             {
                 Fmt::info("Delete deprecated epg...");
