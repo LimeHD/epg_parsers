4c14edc56708694e3367cc7ed2fb271d02cd6b4f
Алексей Анисимов <ork.04@yandex.ru>
2020-07-24 11:41:31 +0300

    stdout для dvbs парсера

--- dvbs_parser/index.php
+++ dvbs_parser/index.php
@@ -12,7 +12,17 @@ if (isset($options["output"])) {
 
 $urlKeepEpgIds = $options["resource"];
 
+$version = "v.0.0.1";
+echo "Парсер stv_parser $version" . PHP_EOL . PHP_EOL;
+
 $parser = new ParserCards($urlKeepEpgIds);
+
 $epgsId = $parser->getAllEpgIDs();
+echo "Собрал все epg_id для парсера c ресурса $urlKeepEpgIds" . PHP_EOL;
+echo "Начинаю парсить телепрограммы" . PHP_EOL;
 $data = $parser->readEpgFromJson($epgsId);
+echo "Cпарсил телепрограммы" . PHP_EOL;
+
 $parser->save($data, $output);
+echo "Сохраняю данные в $output" .PHP_EOL;
+echo "Работа парсера завершена" . PHP_EOL;
