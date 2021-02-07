88e5ffb8409474543f24f2322eb3b75921ebb3a1
zikwall <andrey.kapitonov.96@gmail.com>
2021-02-07 21:03:14 +0300

    Исправление некоторых тестов с учетом UTC+3 [#176837543]

--- kazakh/datetime_test.go
+++ kazakh/datetime_test.go
@@ -1,8 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TesDatetime(t *testing.T) {
+func TestDatetime(t *testing.T) {
 	t.Run("it should be give correct date", func(t *testing.T) {
 		date, err := Date("01.02.2021")
 
@@ -11,15 +13,15 @@ func TesDatetime(t *testing.T) {
 		}
 
 		t.Run("it should be successfully convert date format", func(t *testing.T) {
-			if FormatDatetime(date) != "01.02.2021 00:00:00" {
-				t.Fatal("Failed, expect datetime: `01.02.2021 00:00:00`")
+			if FormatDatetime(date) != "01.02.2021 03:00:00" {
+				t.Fatal("Failed, expect datetime: `01.02.2021 03:00:00`")
 			}
 		})
 
 		_, err = Date("01.02.2021 01:00:00")
 
-		if err != nil {
-			t.Fatal(err)
+		if err == nil {
+			t.Fatal("Failed, expect error")
 		}
 
 		_, err = Date("01-02-2021")
@@ -36,11 +38,17 @@ func TesDatetime(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		datetime := AddTime(date, 2, 5).UTC()
+		datetime := AddTime(date, 2, 5)
+
+		t.Run("it should be correct give UTC time zone", func(t *testing.T) {
+			if datetime.Format(DateTimeFormat) != "07.02.2021 02:05:00" {
+				t.Fatalf("Failed, give %s expect `07.02.2021 05:05:00`", datetime.Format(DateTimeFormat))
+			}
+		})
 
 		t.Run("it should be successfully convert date format", func(t *testing.T) {
-			if FormatDatetime(datetime) != "07.02.2021 02:05:00" {
-				t.Fatal("Failed, expect datetime: `07.02.2021 02:05:00`")
+			if FormatDatetime(datetime) != "07.02.2021 05:05:00" {
+				t.Fatal("Failed, expect datetime: `07.02.2021 05:05:00`")
 			}
 		})
 	})
